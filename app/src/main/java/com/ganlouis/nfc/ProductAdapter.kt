ff1e1be907677d5cfbf254c71623c0eba3442d38
thelouisgan <louisgzy1221@gmail.com>
2024-07-12 19:48:18 +0800

    cards selection display

--- app/src/main/java/com/ganlouis/nfc/ProductAdapter.kt
+++ app/src/main/java/com/ganlouis/nfc/ProductAdapter.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ganlouis.nfc.models.Product
@@ -15,11 +17,33 @@ class ProductAdapter(
     private val onProductSelected: (Product) -> Unit
 ) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
-    class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private var selectedPosition = RecyclerView.NO_POSITION
+
+    inner class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val cardView: MaterialCardView = view.findViewById(R.id.productCardView)
         val imageView: ImageView = view.findViewById(R.id.productImageView)
         val nameTextView: TextView = view.findViewById(R.id.productNameTextView)
         val priceTextView: TextView = view.findViewById(R.id.productPriceTextView)
+
+        fun bind(product: Product, isSelected: Boolean) {
+            nameTextView.text = product.name
+            priceTextView.text = "${product.price} eDots"
+            Glide.with(itemView.context).load(product.imageUrl).into(imageView)
+
+            cardView.background = ContextCompat.getDrawable(
+                itemView.context,
+                if (isSelected) R.drawable.card_background_selected
+                else R.drawable.card_background_unselected
+            )
+
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    setSelectedPosition(position)
+                    onProductSelected(product)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
@@ -29,17 +53,15 @@ class ProductAdapter(
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        val product = products[position]
-        holder.nameTextView.text = product.name
-        holder.priceTextView.text = "${product.price} eDots"
-        Glide.with(holder.imageView.context)
-            .load(product.imageUrl)
-            .into(holder.imageView)
-
-        holder.cardView.setOnClickListener {
-            onProductSelected(product)
-        }
+        holder.bind(products[position], position == selectedPosition)
     }
 
     override fun getItemCount() = products.size
+
+    private fun setSelectedPosition(position: Int) {
+        val previousSelected = selectedPosition
+        selectedPosition = position
+        notifyItemChanged(previousSelected)
+        notifyItemChanged(selectedPosition)
+    }
 }
